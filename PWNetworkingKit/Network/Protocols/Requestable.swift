3981686ddc10d96f6039e91489fc5e8b352e8afa
jinwoong <baseball0020@gmail.com>
2023-10-11 20:57:00 +0900

    feat: define the `Requestable` and `Responsable` protocol.

---
+++ PWNetworkingKit/Network/Protocols/Requestable.swift
@@ -0,0 +1,37 @@
+//
+//  Requestable.swift
+//  PWNetworkingKit
+//
+//  Created by jinwoong Kim on 10/11/23.
+//
+
+import Foundation
+
+public protocol Requestable {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HttpMethod { get }
+    var headers: [HttpHeader]? { get }
+    var body: Data? { get }
+}
+
+public extension Requestable {
+    private func url() throws -> URL {
+        guard let url = URL(string: baseURL) else {
+            throw URLError(.badURL)
+        }
+        
+        return url.appending(path: path)
+    }
+    
+    func buildRequest() throws -> URLRequest {
+        let url = try url()
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+        headers?.forEach { request.addValue($0.value, forHTTPHeaderField: $0.key) }
+        
+        return request
+    }
+}
---
+++ PWNetworkingKit/Network/Protocols/Responsable.swift
@@ -0,0 +1,12 @@
+//
+//  Responsable.swift
+//  PWNetworkingKit
+//
+//  Created by jinwoong Kim on 10/11/23.
+//
+
+import Foundation
+
+public protocol Responsable {
+    associatedtype Response
+}
